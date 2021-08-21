ca696dd2f4a0113ffd76e818da7aad3f3487e374
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2021-08-21 16:45:15 -0500

    More progress.

---
+++ output.md
@@ -0,0 +1,5 @@
+
+|[![](https://serebii.net/swordshield/porygon-zevent.jpg)](lol.com)<br /><br />1<br />|
+| ---- |
+|[![](https://serebii.net/teamchallenge.jpg)](lil.com)<br /><br />2<br />|
+|[![](https://serebii.net/swordshield/summerghostevent.jpg)](lel.com)<br /><br />3<br />|
--- waffle.py
+++ waffle.py
@@ -4,12 +4,39 @@ import numpy
 with open("input.csv", newline='') as file:
     array = list(csv.reader(file))
     
-    # Check Headers.
+    # Headers sanity check.
     for x in array[0]:
         if x not in ["img", "text", "url"]:
             print("Error: header incorrect.")
+        if array[0][0] == "url":
+            print("Error: the first header can't be an URL.")
+            
+    # Make the output file.
+    with open("output.md", 'w') as out:
+        #out.write("lol")
     
-    xlen = len(array[0])
-    for y in range(1, len(array)):
-        for x in range(xlen):
-            print(array[y][x])
+        # Make some loopies.
+        xlen = len(array[0])
+        for y in range(1, len(array)):
+            out.write('\n|')
+            for x in range(xlen):
+                # Images
+                if array[0][x] == "img":
+                    if (array[0][x+1]) == "url":
+                        out.write('[')
+                    out.write("![]("+array[y][x]+')')
+                    if (array[0][x+1]) == "url":
+                        out.write("]("+array[y][x+1]+')')
+                # Text
+                elif array[0][x] == "text":
+                    if (array[0][x+1]) == "url":
+                        out.write('[')
+                    out.write(array[y][x])
+                    if (array[0][x+1]) == "url":
+                        out.write("]("+array[y][x+1]+')')
+                
+                # Line break within the cell.
+                out.write("<br />");
+            out.write('|')
+            if y == 1:
+                out.write("\n| ---- |")
